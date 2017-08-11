39eca155213ee88e97d54830dda17dfad7379765
Vincent Dutat <vdutat@nuxeo.com>
2017-08-11 11:33:38 -0700

    add dummy JS functions referenced in code

---
+++ src/main/resources/web/nuxeo.war/scripts/safeEdit.js
@@ -0,0 +1,13 @@
+(function($) {
+
+  $.fn.cleanupSavedData = function() {
+  }
+
+  $.fn.registerPostRestoreCallBacks = function(postRestoreFct) {
+  }
+
+  $.fn.initSafeEditOnForm = function(key, message) {
+  }
+
+})(jQuery);
+
