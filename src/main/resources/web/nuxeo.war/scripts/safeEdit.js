e860b1f976614657404d3768cd3305907445df65
Vincent Dutat <vdutat@nuxeo.com>
2018-12-03 20:23:45 -0800

    comment console.log()

--- src/main/resources/web/nuxeo.war/scripts/safeEdit.js
+++ src/main/resources/web/nuxeo.war/scripts/safeEdit.js
@@ -1,12 +1,15 @@
 (function($) {
 
   $.fn.cleanupSavedData = function() {
+    //console.log('DUMMY cleanupSavedData');
   }
 
   $.fn.registerPostRestoreCallBacks = function(postRestoreFct) {
+    //console.log('DUMMY registerPostRestoreCallBacks');
   }
 
   $.fn.initSafeEditOnForm = function(key, message) {
+    //console.log('DUMMY initSafeEditOnForm');
   }
 
 })(jQuery);
